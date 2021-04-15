21f9630fa6474b4424b45276c1501b2c9dc9cda2
Cristi <CristiVlad25@users.noreply.github.com>
2021-04-15 15:00:18 +0300

    Add files via upload

---
+++ tldr.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from transformers import pipeline
+
+st.write("# TLDR-izer")
+article = st.text_area('Text to be summarized:', height=200)
+
+if st.button("Do the magic"):
+    summarizer = pipeline("summarization")
+    output = summarizer(article, min_length=50, max_length=90)
+    st.write(output[0]['summary_text'])
